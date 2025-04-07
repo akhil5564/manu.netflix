4fba9a22caa37bc3fb3468ecc1b2a6300d93c6f0
akhil5564 <74394421+akhil5564@users.noreply.github.com>
2025-04-07 17:09:39 +0530

    Add files via upload

--- controller/model/Result.js
+++ controller/model/Result.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Define your schema
 const resultSchema = new mongoose.Schema({
   ticket: { type: String, required: true },
   result: { type: String, required: true, match: /^[0-9]{3}$/ }, // Validate 3-digit result
@@ -8,7 +7,6 @@ const resultSchema = new mongoose.Schema({
   time: { type: String, required: true }, // Store the time slot
 }, { timestamps: true }); // Optionally, you can use timestamps
 
-// Safely check if model already exists
-const Result = mongoose.models.Result || mongoose.model('Result', resultSchema);
+const Result = mongoose.model('Result', resultSchema);
 
 module.exports = Result;
